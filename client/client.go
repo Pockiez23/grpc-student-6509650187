e71eba6496283055d15e735ae77e3dad7e3d1ff3
Pockiez23 <ninerocker_2904@hotmail.com>
2026-03-30 15:05:47 +0700

    feat: implement student service with gRPC definitions and server mock data

---
+++ client/client.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "grpc-student/studentpb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewStudentServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := client.GetStudent(ctx, &pb.StudentRequest{
+		Id: 101,
+	})
+
+	if err != nil {
+		log.Fatalf("Error calling GetStudent: %v", err)
+	}
+
+	log.Printf("Student Info:")
+	log.Printf("ID: %d", res.Id)
+	log.Printf("Name: %s", res.Name)
+	log.Printf("Major: %s", res.Major)
+	log.Printf("Email: %s", res.Email)
+	log.Printf("Phone: %s", res.Phone)
+
+	log.Println("--------------------------------------------------")
+	log.Println("Calling ListStudents...")
+	listRes, err := client.ListStudents(ctx, &pb.Empty{})
+	if err != nil {
+		log.Fatalf("Error calling ListStudents: %v", err)
+	}
+
+	log.Printf("Students List:")
+	for _, student := range listRes.Student {
+		log.Printf("- ID: %d", student.Id)
+		log.Printf("- Name: %s", student.Name)
+		log.Printf("- Major: %s", student.Major)
+		log.Printf("- Email: %s", student.Email)
+		log.Printf("- Phone: %s", student.Phone)
+		log.Printf("--------------------------------------------------")
+	}
+}
